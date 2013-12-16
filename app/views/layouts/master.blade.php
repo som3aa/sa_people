92937a3425d93bf3fba10466f1e9465dfd339d80
Mohammed Adil <mr2all@hotmail.com>
2013-12-17 00:51:55 +0300

    update share button 4

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -46,6 +46,26 @@
 	  ga('send', 'pageview');
 	</script>
 
+
+    <script type="text/javascript">
+    //<![CDATA[
+      (function() {
+        var shr = document.createElement('script');
+        shr.setAttribute('data-cfasync', 'false');
+        shr.src = '//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js';
+        shr.type = 'text/javascript'; shr.async = 'true';
+        shr.onload = shr.onreadystatechange = function() {
+          var rs = this.readyState;
+          if (rs && rs != 'complete' && rs != 'loaded') return;
+          var apikey = '708056eb066583bcfbe34b19e7db936f';
+          try { Shareaholic.init(apikey); } catch (e) {}
+        };
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(shr, s);
+      })();
+    //]]>
+    </script>
+
 </head>
 
 <body>
